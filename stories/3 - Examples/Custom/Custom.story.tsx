7ddcd84be74802ecc20ff9a48f94ef7f73ffa62e
infinite2009 <455429698@qq.com>
2023-08-15 23:30:25 +0800

    chore: modify structure

--- stories/3 - Examples/Custom/Custom.story.tsx
+++ stories/3 - Examples/Custom/Custom.story.tsx
@@ -175,96 +175,30 @@ export const Basic = () => {
         <EditWrapper id="A" childrenId={['A1', 'A2', 'A3', 'A4']}>
           <Item text="A">
             <EditWrapper id="A1">
-              <Item text="A1" />
-            </EditWrapper>
-            <EditWrapper id="A2">
-              <Item text="A2" />
-            </EditWrapper>
-            <EditWrapper id="A3">
-              <Item text="A3" />
-            </EditWrapper>
-            <EditWrapper id="A4">
-              <Item text="A4" />
-            </EditWrapper>
-          </Item>
-        </EditWrapper>
-        <EditWrapper id="B" childrenId={['B1', 'B2', 'B3', 'B4']}>
-          <Item text="B">
-            <EditWrapper id="B1">
-              <Item text="B1" />
-            </EditWrapper>
-            <EditWrapper id="B2">
-              <Item text="B2" />
-            </EditWrapper>
-            <EditWrapper id="B3">
-              <Item text="B3" />
-            </EditWrapper>
-            <EditWrapper id="B4">
-              <Item text="B4" />
+              <Item text="A1" >
+                <EditWrapper id="A11">
+                  <Item text="A11" >
+                    <EditWrapper id="A111">
+                      <Item text="A111" />
+                    </EditWrapper>
+                  </Item>
+                </EditWrapper>
+              </Item>
             </EditWrapper>
           </Item>
         </EditWrapper>
         <EditWrapper id="C" childrenId={['C1', 'C2', 'C3', 'C4']}>
           <Item text="C">
             <EditWrapper id="C1">
-              <Item text="C1" />
-            </EditWrapper>
-            <EditWrapper id="C2">
-              <Item text="C2" />
-            </EditWrapper>
-            <EditWrapper id="C3">
-              <Item text="C3" />
-            </EditWrapper>
-            <EditWrapper id="C4">
-              <Item text="C4" />
-            </EditWrapper>
-          </Item>
-        </EditWrapper>
-        <EditWrapper id="D" childrenId={['D1', 'D2', 'D3', 'D4']}>
-          <Item text="D">
-            <EditWrapper id="D1">
-              <Item text="D1" />
-            </EditWrapper>
-            <EditWrapper id="D2">
-              <Item text="D2" />
-            </EditWrapper>
-            <EditWrapper id="D3">
-              <Item text="D3" />
-            </EditWrapper>
-            <EditWrapper id="D4">
-              <Item text="D4" />
-            </EditWrapper>
-          </Item>
-        </EditWrapper>
-        <EditWrapper id="E" childrenId={['E1', 'E2', 'E3', 'E4']}>
-          <Item text="E">
-            <EditWrapper id="E1">
-              <Item text="E1" />
-            </EditWrapper>
-            <EditWrapper id="E2">
-              <Item text="E2" />
-            </EditWrapper>
-            <EditWrapper id="E3">
-              <Item text="E3" />
-            </EditWrapper>
-            <EditWrapper id="E4">
-              <Item text="E4" />
-            </EditWrapper>
-          </Item>
-        </EditWrapper>
-        <EditWrapper id="F" childrenId={['F1', 'F2', 'F3', 'F4']}>
-          <Item text="F">
-            <EditWrapper id="F1">
-              <Item text="F1" />
-            </EditWrapper>
-            <EditWrapper id="F2">
-              <Item text="F2" />
-            </EditWrapper>
-            <EditWrapper id="F3">
-              <Item text="F3" />
-            </EditWrapper>
-            <EditWrapper id="F4">
-              <Item text="F4" />
+              <Item text="C1" >
+                <EditWrapper id="C11">
+                  <Item text="C11" >
+                    <EditWrapper id="C111" >
+                      <Item text="C111" />
+                    </EditWrapper>
+                  </Item>
+                </EditWrapper>
+              </Item>
             </EditWrapper>
           </Item>
         </EditWrapper>
